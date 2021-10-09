75d03497d1b7d209eb8322912cf8fc71e9156448
Shaurya Aditya Singh <59340919+Shaurya111001@users.noreply.github.com>
2021-10-09 09:57:19 +0530

    Update signupForm.jsx

--- src/FrontPage/accountBox/signupForm.jsx
+++ src/FrontPage/accountBox/signupForm.jsx
@@ -1,12 +1,19 @@
 import React, {useContext} from 'react'
 import { BoldLink, BoxContainer, FormContainer, Input, MutedLink, SubmitButton } from "./coomon";
 import {AccountContext} from "./accountContext"
+import axios from 'axios';
 function SignupForm() {
     const { switchToSignin } =useContext(AccountContext);
+    const signup = () => {
+        axios.post()
+    }
+    const [email, setemail] = useState("")
+    const [password, setpass] = useState("")
+
     return (
         <BoxContainer>
             <FormContainer>
-                <Input type="text" placeholder ="Full Name" />
+                <Input type="text" value ={email} placeholder ="Full Name" />
                 <Input type="email" placeholder ="Email" />
                 <Input type="password" placeholder ="Password" />
                 <Input type="password" placeholder =" Confirm Password" />
